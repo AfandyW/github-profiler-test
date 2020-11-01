38672dfc1d80d04c30b8c0feabef7dae083454eb
AfandyW <afandywibowo2000@gmail.com>
2020-11-01 11:20:35 +0800

    request data api github done

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # GitHub Profiler
 
 problem: we need a simple to look at github profile
-solution: use nodeJS to connect to github api
-get profile into and print out to the console
+solution: use nodeJS to connect to github api get profile into and print out to the console
 
 ## TODO
 * [x] connect to github API
-* Read the data
-* TODO: Parse the data
-* TODO: Print the data out
+* [x] Read the data
+* [x] TODO: Parse the data
+* [x] TODO: Print the data out
--- index.js
+++ index.js
@@ -1,26 +1,42 @@
-
-
 const https = require('https')
 
+let username = 'Afandy'
+
+// [x] connect to github API
 let options = {
-    'hostname': 'ai.github.com',
+    'hostname': 'api.github.com',
     'port': 443,
-    'path': '/users/yofri',
+    'path': `/users/${username}`,
     'method': 'GET',
     'headers': {
         'user-agent': 'nodejs'
     }
 }
-
-let request = https.request(options, (result)=>{
-    console.log('Got response: ', result.statusCode)
+// [x] Read the data
+let request = https.request(options, (res)=>{
+    if (res.statusCode === 200){
+        let body = ''
+        res.on('data',(data) => {
+            body = body + data
+        })
+        // [x] TODO: Parse the data
+        res.on('end', ()=>{
+            let data = JSON.parse(body)
+            console.log(data)
+        })
+    }else{
+        console.log(`
+        Username ${username} Not Found
+        Error Code: , ${res.statusCode}`)
+    }
 })
 
 request.end()
+request.on('error', (e) => {
+    console.error(e)
+})
 
-// request.on('error', (e) => {
-//     console.error(e)
-// })
+
 
 
 
